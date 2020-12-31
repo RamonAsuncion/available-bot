2eca0d3da5e3f8196f58936a8339b68db3c31dbc
Ramon Asuncion <asuncionbatista@gmail.com>
2020-12-30 23:52:10 -0500

    Time tracked added

---
+++ mainbot.py
@@ -0,0 +1,14 @@
+import time 
+
+# Start time of program
+start_time = time.time()
+
+def main():
+    pass
+    
+if __name__ == '__main__':
+    main()
+
+
+# End time of program    
+print(f'Seconds to run: {time.time() - start_time}')
